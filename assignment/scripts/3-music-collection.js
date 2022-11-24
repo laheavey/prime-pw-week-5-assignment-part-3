7c3decc5b3a8daf8ffd97edcc14f2155e8624945
Lauren <lauren.a.heavey@gmail.com>
2022-11-23 18:14:16 -0600

    Add answers thru addToCollection function.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,24 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection (title, artist, yearPublished) {
+    let album = {
+        title,
+        artist,
+        yearPublished,
+    }
+    collection.push(album);
+    return album;
+}
+
+console.log(addToCollection ("Sainthood", "Tegan and Sara", 2009));
+
+console.log(addToCollection ("Waterloo", "ABBA", 1974));
+console.log(addToCollection ("Brothers", "The Black Keys", 2010));
+console.log(addToCollection ("No Burden", "Lucy Dacus", 2016));
+console.log(addToCollection ("Treats", "Sleigh Bells", 2010));
+console.log(addToCollection ("Arrival", "ABBA", 1976));
+console.log(addToCollection ("Heartthrob", "Tegan and Sara", 2013));
+
+console.log(collection);
