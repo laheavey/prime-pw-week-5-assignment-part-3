2eb322d91c73cd9413d66a8cae443dcb63462295
Lauren <lauren.a.heavey@gmail.com>
2022-11-27 11:35:12 -0600

    Add work through stretch problems, still incomplete.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,9 +1,11 @@
 console.log('***** Music Collection *****')
 
+//Required Questions
+
 let collection = [];
 
 function addToCollection (title, artist, yearPublished) {
-    let album = {
+    const album = {
         title,
         artist,
         yearPublished,
@@ -13,20 +15,18 @@ function addToCollection (title, artist, yearPublished) {
 }
 
 console.log(addToCollection ("Sainthood", "Tegan and Sara", 2009));
-
 console.log(addToCollection ("Waterloo", "ABBA", 1974));
 console.log(addToCollection ("Brothers", "The Black Keys", 2010));
 console.log(addToCollection ("No Burden", "Lucy Dacus", 2016));
 console.log(addToCollection ("Treats", "Sleigh Bells", 2010));
 console.log(addToCollection ("Arrival", "ABBA", 1976));
 console.log(addToCollection ("Heartthrob", "Tegan and Sara", 2013));
-
 console.log(collection);
 
 function showCollection (array){
-    console.log(array.length-1);
+    console.log('Collection length: ' + (array.length-1) + ' Albums');
     for (i in array){
-        object = array[i];
+        let object = array[i];
         console.log(object.title + " by " + object.artist + ", published in " + object.yearPublished);
     }
 }
@@ -45,4 +45,53 @@ function findByArtist(artist) {
 
 console.log(findByArtist("ABBA"));
 console.log(findByArtist("Tierra Whack"));
-//console.log(collection[0].artist);
+
+//Stretch Goals
+
+function search (artist, year) {
+    let results = [];
+    for (let i=0; i<collection.length; i++){
+        if (artist || year) {
+            if (collection[i].artist === artist && collection[i].yearPublished === year){
+                console.log("Match found!");
+                results.push(collection[i]);
+            } else {
+                results = [];
+            }
+        } else {
+            console.log("Empty input.");
+            results = collection;
+        }
+    }
+    return results;
+}
+
+//addToCollection ("", "Ray Charles", 1907);
+addToCollection ("", "Ray Charles", 1957);
+
+//console.log(search()); 
+//console.log(search("Ray Charles")); 
+//console.log(search("Ray Charles", 1957));
+
+
+
+
+// collection = array
+//album = object
+//title = property (string)
+//artist = property (string)
+//yearPublished = property (numerical)
+//trackList = array
+//track = object 
+//trackName = property (string)
+//trackDuration = property (string)
+
+let tracklist = [];
+
+function addTracks (trackName, trackDuration) {
+    tracklist.push({trackName,trackDuration});
+}
+ function updateCollection (tracklist) {
+    let tracklist = [];
+    for (let )
+ }
